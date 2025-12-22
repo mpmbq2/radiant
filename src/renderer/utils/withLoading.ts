b6f3bd744162ac9861724d187f550ddd25a79bbb
Matthew McCabe <mpmbq2@gmail.com>
2025-12-22 11:58:02 -0500

    Found some orphan changes that didn't make it into a commit :|

--- src/renderer/utils/withLoading.ts
+++ src/renderer/utils/withLoading.ts
@@ -13,6 +13,7 @@ const logger = createLogger('withLoading');
  * @param operation - The async operation to wrap
  * @param operationName - Name of the operation for logging purposes
  * @param options - Configuration options
+ * @param options.silent - When true, does not set isLoading state (for background operations)
  * @returns The result of the operation or void if an error occurred and rethrow is false
  */
 export const withLoading = async <
@@ -22,9 +23,11 @@ export const withLoading = async <
   set: (state: Partial<S> | ((state: S) => Partial<S>)) => void,
   operation: () => Promise<T>,
   operationName = 'Operation',
-  options: { rethrow?: boolean } = {}
+  options: { rethrow?: boolean; silent?: boolean } = {}
 ): Promise<T | void> => {
-  set({ isLoading: true, error: null } as Partial<S>);
+  if (!options.silent) {
+    set({ isLoading: true, error: null } as Partial<S>);
+  }
   try {
     return await operation();
   } catch (error) {
@@ -38,6 +41,8 @@ export const withLoading = async <
       throw error;
     }
   } finally {
-    set({ isLoading: false } as Partial<S>);
+    if (!options.silent) {
+      set({ isLoading: false } as Partial<S>);
+    }
   }
 };
